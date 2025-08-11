af322962cff44c63a1cd679781c08b765cb479d4
Aditi Sharma <aditi.sharma@leaseplan.com>
2025-08-11 13:06:43 +0200

    update

--- src/test/java/stepDefinitions/CommonStepDefinitions.java
+++
@@ -1,9 +0,0 @@
-package stepDefinitions;
-
-import io.cucumber.java.en.Then;
-import io.restassured.response.Response;
-import net.serenitybdd.core.Serenity;
-
-public class CommonStepDefinitions {
-   
-}
